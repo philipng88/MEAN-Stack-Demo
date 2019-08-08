74909b91dbf5f81c753ea02971065fd73fccea98
philipng88 <philiplyng@gmail.com>
2019-08-07 19:00:43 -0600

    added Node backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
 import { 
   MatInputModule, 
   MatCardModule, 
@@ -31,6 +32,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatButtonModule, 
     MatToolbarModule, 
     MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
