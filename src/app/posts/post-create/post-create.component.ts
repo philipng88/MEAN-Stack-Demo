f52e3b2f8956d09aeedd7caf0e51efc9b1c6cdb6
philipng88 <philiplyng@gmail.com>
2019-08-25 14:15:34 -0600

    added image preview and validation

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator'
 
 @Component({
     selector: 'app-post-create',
@@ -16,6 +17,7 @@ export class PostCreateComponent implements OnInit {
     post: Post
     isLoading = false
     form: FormGroup
+    imagePreview: string
     private mode = 'create'
     private postId: string
 
@@ -24,7 +26,8 @@ export class PostCreateComponent implements OnInit {
     ngOnInit() {
         this.form = new FormGroup({
             title: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
-            content: new FormControl(null, {validators: [Validators.required]})
+            content: new FormControl(null, {validators: [Validators.required]}),
+            image: new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
         })
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
             if (paramMap.has('postId')) {
@@ -41,6 +44,17 @@ export class PostCreateComponent implements OnInit {
                 this.postId = null
             }
         })
+    }
+
+    onImagePicked(event: Event) {
+        const file = (event.target as HTMLInputElement).files[0]
+        this.form.patchValue({image: file})
+        this.form.get('image').updateValueAndValidity()
+        const reader = new FileReader()
+        reader.onload = () => {
+            this.imagePreview = reader.result as string
+        }
+        reader.readAsDataURL(file)
     }
 
     onSavePost() {
